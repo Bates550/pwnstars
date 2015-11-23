2acf2cef8280279a0dda7eb482501f7f7ea09ec5
Sean Spearman <seanmspearman@gmail.com>
2015-11-22 17:13:54 -0800

    Initial commit.

---
+++ create.sql
@@ -0,0 +1,10 @@
+DROP DATABASE PROJECT;
+CREATE DATABASE project;
+USE project;
+
+CREATE TABLE Employees(
+	id INTEGER,
+	age INTEGER,
+	salary LONGBLOB NOT NULL, # Bob Loblaw's LONGBLOB
+	PRIMARY KEY(id)
+);
